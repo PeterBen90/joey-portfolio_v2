d195d8d43e43b1c981829b72f57f6a7cd03c052c
Peter Benvenuto <peterbenvenuto90@gmail.com>
2020-12-22 23:12:31 -0500

    about page done

--- assets/js/about.js
+++ assets/js/about.js
@@ -9,3 +9,16 @@ hamburgerButton.addEventListener("click", (e) => {
 	hamburgerButton.classList.toggle("active");
 	overlay.classList.toggle("visible");
 });
+
+$('a[href^="#"]').on("click", function (event) {
+	var target = $(this.getAttribute("href"));
+	if (target.length) {
+		event.preventDefault();
+		$("html, body").stop().animate(
+			{
+				scrollTop: target.offset().top,
+			},
+			1000
+		);
+	}
+});
