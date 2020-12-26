91860cdee177e633c0f341a662faed60325e2479
Peter Benvenuto <peterbenvenuto90@gmail.com>
2020-12-26 04:37:58 -0500

    second project option

--- assets/js/about.js
+++ assets/js/about.js
@@ -22,3 +22,18 @@ $('a[href^="#"]').on("click", function (event) {
 		);
 	}
 });
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+	panel.addEventListener("click", () => {
+		removeActiveClasses();
+		panel.classList.add("active");
+	});
+});
+
+function removeActiveClasses() {
+	panels.forEach((panel) => {
+		panel.classList.remove("active");
+	});
+}
